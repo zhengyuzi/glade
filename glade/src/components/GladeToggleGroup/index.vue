db6da95ce1c8e1c05b86ea52dd65cc4ca3669cba
zhengyuzi <381740310@qq.com>
2025-03-10 16:32:53 +0800

    feat: add GladeToggleGroup

---
+++ glade/src/components/GladeToggleGroup/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { IToggleGroup } from '@/types'
+import { ToggleGroupItem, ToggleGroupRoot } from 'reka-ui'
+
+interface IProps {
+  options: IToggleGroup[]
+  ariaLabel: string
+}
+
+withDefaults(defineProps<IProps>(), {
+  options: () => [],
+  ariaLabel: undefined,
+})
+</script>
+
+<template>
+  <ToggleGroupRoot class="space-x-2 py-1">
+    <ToggleGroupItem
+      v-for="item in options"
+      :key="item.value"
+      :value="item.value"
+      :aria-label="ariaLabel"
+      class="p-2 rounded bg-gray-200 data-[state=on]:(bg-gray-400 text-white)"
+    >
+      <component :is="item.content" />
+    </ToggleGroupItem>
+  </ToggleGroupRoot>
+</template>
