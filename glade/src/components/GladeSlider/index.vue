d0e89fb892f590eddd2b5319f335031f301abf8c
zhengyuzi <381740310@qq.com>
2025-03-10 16:32:23 +0800

    feat: update GladeSlider props

--- glade/src/components/GladeSlider/index.vue
+++ glade/src/components/GladeSlider/index.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
 import { SliderRange, SliderRoot, SliderThumb, SliderTrack } from 'reka-ui'
+
+interface IProps {
+  max?: number
+  min?: number
+  step?: number
+}
+
+withDefaults(defineProps<IProps>(), {
+  max: 100,
+  min: 0,
+  step: 1,
+})
 </script>
 
 <template>
   <SliderRoot
-    class="relative flex items-center select-none touch-none w-full h-8"
-    :max="100"
-    :step="1"
+    class="relative flex items-center select-none touch-none w-full h-6"
+    :max="max"
+    :min="min"
+    :step="step"
   >
-    <SliderTrack class="bg-gray-200 relative grow rounded-full h-2.5">
+    <SliderTrack class="bg-gray-200 relative grow rounded-full h-2">
       <SliderRange class="absolute bg-gray-400 rounded-full h-full" />
     </SliderTrack>
     <SliderThumb
-      class="block w-6 h-6 bg-white border-4 border-gray-500 rounded-full shadow-md cursor-pointer"
+      class="block w-5 h-5 bg-white border-4 border-gray-500 rounded-full shadow-md cursor-pointer"
       aria-label="Volume"
     />
   </SliderRoot>
