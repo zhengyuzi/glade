2f6abd33a8f84223412737f125424e024eb5ca34
zhengyuzi <381740310@qq.com>
2025-03-18 11:41:56 +0800

    chore: update GladePopover component

--- glade/src/components/GladePopover/index.vue
+++ glade/src/components/GladePopover/index.vue
@@ -9,10 +9,12 @@ import {
 
 interface IProps {
   side?: 'right' | 'bottom' | 'left' | 'top'
+  sideOffset?: number
 }
 
 withDefaults(defineProps<IProps>(), {
   side: 'right',
+  sideOffset: 20,
 })
 </script>
 
@@ -23,11 +25,17 @@ withDefaults(defineProps<IProps>(), {
     </PopoverTrigger>
     <PopoverPortal>
       <PopoverContent
-        class="z-1000 min-w-250px rounded-lg p-5 bg-white shadow-[0px_10px_35px_0px_rgba(22,_23,_24,_0.1),_0px_10px_10px_-10px_rgba(22,_23,_24,_0.1)]"
-        :side-offset="25"
+        class="z-1000 min-w-250px rounded-lg bg-white shadow-[0px_10px_35px_0px_rgba(22,_23,_24,_0.1),_0px_10px_10px_-10px_rgba(22,_23,_24,_0.1)] border-2 border-gray-300 focus:outline-none"
+        :side-offset="sideOffset"
         :side="side"
       >
-        <PopoverArrow :width="12" :height="8" class="fill-white stroke-gray-100" />
+        <span class="absolute left-2px">
+          <PopoverArrow
+            :width="12"
+            :height="8"
+            class="fill-white stroke-2 stroke-gray-300"
+          />
+        </span>
         <slot name="content" />
       </PopoverContent>
     </PopoverPortal>
