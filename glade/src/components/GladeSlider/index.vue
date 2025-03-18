2b58e095284756d405f7dfd9c589e280affadd5d
zhengyuzi <381740310@qq.com>
2025-03-18 11:42:15 +0800

    chore: update GladeSlider component

--- glade/src/components/GladeSlider/index.vue
+++ glade/src/components/GladeSlider/index.vue
@@ -5,27 +5,37 @@ interface IProps {
   max?: number
   min?: number
   step?: number
+  value: number[]
 }
 
 withDefaults(defineProps<IProps>(), {
   max: 100,
   min: 0,
   step: 1,
+  value: () => [],
 })
+
+const emit = defineEmits(['update:value'])
+
+function updateValue(value?: number[]) {
+  emit('update:value', value || [])
+}
 </script>
 
 <template>
   <SliderRoot
-    class="relative flex items-center select-none touch-none w-full h-6"
+    class="flex-1 relative flex items-center select-none touch-none"
     :max="max"
     :min="min"
     :step="step"
+    :model-value="value"
+    @update:model-value="updateValue"
   >
     <SliderTrack class="bg-gray-200 relative grow rounded-full h-2">
       <SliderRange class="absolute bg-gray-400 rounded-full h-full" />
     </SliderTrack>
     <SliderThumb
-      class="block w-5 h-5 bg-white border-4 border-gray-500 rounded-full shadow-md cursor-pointer focus:outline-none"
+      class="block w-4 h-4 bg-white border-3 border-gray-500 rounded-full shadow-md cursor-pointer focus:outline-none"
       aria-label="Volume"
     />
   </SliderRoot>
