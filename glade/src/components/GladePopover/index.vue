912a419d2c90c170a69c68c9cf032cff3b7cf9be
zhengyuzi <381740310@qq.com>
2025-03-19 16:37:32 +0800

    chore: update style

--- glade/src/components/GladePopover/index.vue
+++ glade/src/components/GladePopover/index.vue
@@ -25,17 +25,15 @@ withDefaults(defineProps<IProps>(), {
     </PopoverTrigger>
     <PopoverPortal>
       <PopoverContent
-        class="z-1000 min-w-250px rounded-lg bg-white shadow-[0px_10px_35px_0px_rgba(22,_23,_24,_0.1),_0px_10px_10px_-10px_rgba(22,_23,_24,_0.1)] border-2 border-gray-300 focus:outline-none"
+        class="relative z-9 min-w-250px rounded-lg bg-white shadow-[0px_10px_35px_0px_rgba(22,_23,_24,_0.1),_0px_10px_10px_-10px_rgba(22,_23,_24,_0.1)] border-2 border-gray-300 focus:outline-none"
         :side-offset="sideOffset"
         :side="side"
       >
-        <span class="absolute left-2px">
-          <PopoverArrow
-            :width="12"
-            :height="8"
-            class="fill-white stroke-2 stroke-gray-300"
-          />
-        </span>
+        <PopoverArrow
+          :width="12"
+          :height="8"
+          class="fill-white stroke-2 stroke-gray-300"
+        />
         <slot name="content" />
       </PopoverContent>
     </PopoverPortal>
--- glade/src/panel/index.vue
+++ glade/src/panel/index.vue
@@ -9,11 +9,11 @@ import Zoom from './Zoom/index.vue'
 <template>
   <div class="glade-control-panel pointer-events-none">
     <Header class="absolute z-1 top-6 left-6 right-6" />
-    <Toolbar class="pointer-events-auto" />
+    <Toolbar class="fixed z-2 touch-none pointer-events-auto" />
     <ToolTab class="pointer-events-auto absolute z-1 top-30 right-6" />
-    <div class="absolute z-1 right-6 bottom-6 flex items-center space-x-4 children:(h-12)">
-      <History class="pointer-events-auto" />
-      <Zoom class="pointer-events-auto" />
+    <div class="absolute z-1 right-6 bottom-6 flex items-center space-x-4">
+      <History class="h-11.5 pointer-events-auto" />
+      <Zoom class="h-11.5 pointer-events-auto" />
     </div>
   </div>
 </template>
